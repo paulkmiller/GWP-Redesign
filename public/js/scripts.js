4296b1c6ab01c68b6959ebf4c1976826edc992e6
Paul Miller <pmiller@ASH0514LM.local>
2016-07-29 16:12:32 -0400

    Maps work, testing images before revert

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 
     /***************** Mapbox Layer ******************/
 
-  L.mapbox.accessToken = 'pk.eyJ1IjoicG1pbGxlcmsiLCJhIjoiY2lyNnViOWkzMDBsa2c4bms4MXlpcmNxciJ9.qG5rdqfa7u6BNu8xHHcX4w';
-  L.mapbox.map('map-one', 'mapbox.streets').setView([38.94, -77.32], 14);
+    L.mapbox.accessToken = 'pk.eyJ1IjoicG1pbGxlcmsiLCJhIjoiY2lyNnViOWkzMDBsa2c4bms4MXlpcmNxciJ9.qG5rdqfa7u6BNu8xHHcX4w';
+    L.mapbox.map('map-one', 'mapbox.streets').setView([38.94, -77.32], 14);
+
 
     /***************** Like Counter ******************/
 
