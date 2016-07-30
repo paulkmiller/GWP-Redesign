c5dd20918ed429d54775928153963fb0ac83d9cc
Paul Miller <pmiller@ASH0514LM.fios-router.home>
2016-07-30 17:44:23 -0400

    Added contact page; initial map + overlay stable

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -35,72 +35,75 @@ $(document).ready(function() {
         interactive: false,
         zoom: 14
     });
-    //
-    // var framesPerSecond = 15;
-    // var initialOpacity = 1
-    // var opacity = initialOpacity;
-    // var initialRadius = 6;
-    // var radius = initialRadius;
-    // var maxRadius = 18;
-    //
-    // map.on('load', function () {
-    //
-    //     // Add a source and layer displaying a point which will be animated in a circle.
-    //     map.addSource('point', {
-    //         "type": "geojson",
-    //         "data": {
-    //             "type": "Point",
-    //             "coordinates": [
-    //                 settings.long, settings.lat
-    //             ]
-    //         }
-    //     });
-    //
-    //     map.addLayer({
-    //         "id": "point",
-    //         "source": "point",
-    //         "type": "circle",
-    //         "paint": {
-    //             "circle-radius": initialRadius,
-    //             "circle-radius-transition": {duration: 0},
-    //             "circle-opacity-transition": {duration: 0},
-    //             "circle-color": "#007cbf"
-    //         }
-    //     });
-    //
-    //     map.addLayer({
-    //         "id": "point1",
-    //         "source": "point",
-    //         "type": "circle",
-    //         "paint": {
-    //             "circle-radius": initialRadius,
-    //             "circle-color": "#007cbf"
-    //         }
-    //     });
-    //
-    //
-    //     function animateMarker(timestamp) {
-    //         setTimeout(function(){
-    //             requestAnimationFrame(animateMarker);
-    //
-    //             radius += (maxRadius - radius) / framesPerSecond;
-    //             opacity -= ( .9 / framesPerSecond );
-    //
-    //             map.setPaintProperty('point', 'circle-radius', radius);
-    //             map.setPaintProperty('point', 'circle-opacity', opacity);
-    //
-    //             if (opacity <= 0) {
-    //                 radius = initialRadius;
-    //                 opacity = initialOpacity;
-    //             }
-    //
-    //         }, 1000 / framesPerSecond);
-    //
-    //     }
-    //
-    //     // Start the animation.
-    //     animateMarker(0);
-    // });
+
+    // Pulse Dot Experiment
+
+    var framesPerSecond = 15;
+    var initialOpacity = 1
+    var opacity = initialOpacity;
+    var initialRadius = 6;
+    var radius = initialRadius;
+    var maxRadius = 18;
+
+    map.on('load', function () {
+
+        // Add a source and layer displaying a point which will be animated in a circle.
+        map.addSource('point', {
+            "type": "geojson",
+            "data": {
+                "type": "Point",
+                "coordinates": [
+                    settings.long, settings.lat
+                ]
+            }
+        });
+
+        map.addLayer({
+            "id": "point",
+            "source": "point",
+            "type": "circle",
+            "paint": {
+                "circle-radius": initialRadius,
+                "circle-radius-transition": {duration: 0},
+                "circle-opacity-transition": {duration: 0},
+                "circle-color": "#007cbf"
+            }
+        });
+
+        map.addLayer({
+            "id": "point1",
+            "source": "point",
+            "type": "circle",
+            "paint": {
+                "circle-radius": initialRadius,
+                "circle-color": "#007cbf"
+            }
+        });
+
+
+        // Animate the point
+        function animateMarker(timestamp) {
+            setTimeout(function(){
+                requestAnimationFrame(animateMarker);
+
+                radius += (maxRadius - radius) / framesPerSecond;
+                opacity -= ( .9 / framesPerSecond );
+
+                map.setPaintProperty('point', 'circle-radius', radius);
+                map.setPaintProperty('point', 'circle-opacity', opacity);
+
+                if (opacity <= 0) {
+                    radius = initialRadius;
+                    opacity = initialOpacity;
+                }
+
+            }, 1000 / framesPerSecond);
+
+        }
+
+        // Start the animation.
+        animateMarker(0);
+    });
 
 
     /***************** Search Component ******************/
